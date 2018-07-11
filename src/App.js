ffff925dff36303b6886a8060d0992ef28c17e50
Mightynumnums <deadcookies@gmail.com>
2018-07-11 16:56:45 -0400

    add components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import './App.css';
 //import github from './images/github.svg'
 import Footer from './components/Footer.js'
 import Main from './components/Main.js'
+import Navbar from './components/Navbar.js'
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <header className="header">
-          <p>this will be a navbar</p>
-        </header>
+        <Navbar />
         <Main />
         <Footer />
       </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -10,7 +10,7 @@ export default class Main extends Component {
         <div className='bio'>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas hendrerit elit nec pellentesque interdum. Praesent sed mauris nibh. Fusce facilisis eleifend lectus. Proin pulvinar nunc non nunc vestibulum, id suscipit leo dignissim. Nullam id lectus varius, ultricies justo et, maximus est. Duis nec eleifend orci, id iaculis urna. Etiam vel risus eget elit varius ornare in sit amet leo. Mauris non ultrices lorem. Vivamus sem sapien, accumsan eu dui id, commodo mattis lacus. Ut quis condimentum velit. Quisque mi ligula, consectetur id nisi sit amet, semper lobortis ante. Nullam malesuada id diam ultricies congue. Fusce sem diam, egestas vitae elit id, ultricies fermentum dolor. Etiam lobortis condimentum erat a pretium. Donec a orci tellus.
           </p>
-          <img src={profilePic} className="profile" alt="selfImg" />
+          <img src={profilePic} className="profile bio" alt="selfImg" />
         </div>
       </div>
     );
