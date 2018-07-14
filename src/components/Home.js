ff0218aab01dc5fd595fba72420113efec790158
Mightynumnums <deadcookies@gmail.com>
2018-07-14 14:37:15 -0400

    add projects styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,9 +7,9 @@ const Home = () => {
   return (
     <div className="main">
       <div className='bio'>
-        <p>Hi there! I'm Aleks and I am a software developer with a keen eye for design.
+        <p id='bioText'>Hi there! I'm Aleks and I am a software developer with a keen eye for design.
         varius, ultricies justo et, maximus est. Duis nec eleifend orci, id iaculis urna. Etiam vel risus eget elit varius ornare in sit amet leo.
-        <br /> Varius, ultricies justo et, maximus est. Duis nec eleifend orci, id iaculis urna. Etiam vel risus eget elit varius ornare in sit amet leo. Mauris non ultrices lorem. Vivamus sem sapien, accumsan eu dui id, commodo mattis lacus. Ut quis condimentum velit. Quisque mi ligula, consectetur id nisi sit amet, semper lobortis ante. Nullam malesuada id diam ultricies congue. Fusce sem diam, egestas vitae elit id, ultricies fermentum dolor. Etiam lobortis condimentum erat a pretium. Donec a orci tellus.
+        <br /> Varius, ultricies justo et, maximus est. Duis nec eleifend orci, id iaculis urna. Etiam vel risus eget elit varius ornare in sit amet leo. Mauris non ultrices lorem. Vivamus sem sapien, accumsan eu dui id, commodo mattis
         <br />
           Mauris non ultrices lorem. Vivamus sem sapien, accumsan eu dui id, commodo mattis lacus. Ut quis condimentum velit. Quisque mi ligula, consectetur id nisi sit amet, semper lobortis ante. Nullam malesuada id diam ultricies congue. Fusce sem diam, egestas vitae elit id, ultricies fermentum dolor. Etiam lobortis condimentum erat a pretium. Donec a orci tellus.
           </p>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,20 +2,42 @@ import React from 'react'
 import './styles/projects.css'
 import anbeI from '../images/anbeI.jpg'
 import anbeL from '../images/anbeCom.jpg'
+// import donke from '../images/donke.jpg'
 
 const Projects = () => {
   return (
     <div className="projects">
+
       <div id='anbePortfolio'>
         <p id='anbeText'>Anbe Baccam is an emerging photographer, and I really wanted to showcase his work in a simple way to allow the beauty of his photography to speak for itself, without too much destruction and noise.
         </p>
         <div className='images'>
-          <img src={anbeI} id='anbeIphone' alt='iphoneImage' />
           <img src={anbeL} id="anbeComputer" alt="laptopImage" />
+          <img src={anbeI} id='anbeIphone' alt='iphoneImage' />
+        </div>
+        <div className='projLinks'>
+          <a href='https://github.com/Mightynumnums/anbe2'>GITHUB</a>
+          <a href='https://anbebaccamportfolio.herokuapp.com/'>WEBSITE</a>
+        </div>
+
+
+
+        {/* donke project*/}
+        <div id='donke'>
+          <p id='donkeText'>The creation of Donke was inspired by inflamed backs and asleep legs.
+        </p>
+          <div className='images'>
+            {/* <img src={donke} id="donkeDesktop" alt="donkeImg" /> */}
+            <img src={anbeL} id="anbeComputer" alt="laptopImage" />
+          </div>
+          <div className='projLinks'>
+            <a href='https://github.com/teamCrabby/donke'>GITHUB</a>
+            <a href='https://buddy-4f263.firebaseapp.com/'>WEBSITE</a>
+          </div>
         </div>
       </div>
 
-    </div >
+    </div>
   );
 }
 export default Projects
