1aa8da0d35b78d05469da1f694d3b0d9eaeb4715
Mightynumnums <deadcookies@gmail.com>
2018-07-18 21:42:00 -0400

    fix navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,12 +7,42 @@ const Home = () => {
     <div className="main">
       <div className='bio'>
         <p id='bioText'>Hi there! I'm Aleks and I am a software developer with a keen eye for design.
-        varius, ultricies justo et, maximus est. Duis nec eleifend orci, id iaculis urna. Etiam vel risus eget elit varius ornare in sit amet leo.
-        <br /> Varius, ultricies justo et, maximus est. Duis nec eleifend orci, id iaculis urna. Etiam vel risus eget elit varius ornare in sit amet leo. Mauris non ultrices lorem. Vivamus sem sapien, accumsan eu dui id, commodo mattis
+        Originally from Russia, I've been told that I have a tone and a bit of tude. None of those things are true, all I want to do is make cool pretty looking shit.
         <br />
-          Mauris non ultrices lorem. Vivamus sem sapien, accumsan eu dui id, commodo mattis lacus. Ut quis condimentum velit. Quisque mi ligula, consectetur id nisi sit amet, semper lobortis ante. Nullam malesuada id diam ultricies congue. Fusce sem diam, egestas vitae elit id, ultricies fermentum dolor. Etiam lobortis condimentum erat a pretium. Donec a orci tellus.
+          I am a fullstack JavaScript developer keen on working with excited and passionate individuals who want to get things done and are interested in making this dirty world just a little bit more beautiful and a little bit more smart.
           </p>
         <img src={profilePic} className="profile bio" alt="selfImg" />
+
+        <div className='skills'>
+          <ul>
+            <li>JavaScript</li>
+            <li>Node.js</li>
+            <li>Express</li>
+            <li>Sequelize</li>
+            <li>PostreSQL</li>
+          </ul>
+          <ul>
+            <li>React.js</li>
+            <li>Redux</li>
+            <li>React-Redux</li>
+          </ul>
+
+          <ul>
+            <li>HTML5</li>
+            <li>CSS</li>
+            <li>FlexBox</li>
+            <li>Green Sock Library</li>
+          </ul>
+          <ul>
+            <li>Git</li>
+            <li>GitHub</li>
+            <li>Webpack</li>
+          </ul>
+          <ul>
+            <li>Adobe Illustrator</li>
+            <li>Adobe Photoshop</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
