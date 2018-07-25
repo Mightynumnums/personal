37ea993203295e2ba214cbb5e1267f196a6fb3ba
Mightynumnums <deadcookies@gmail.com>
2018-07-25 17:01:59 -0400

    move Page logic into navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,63 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './styles/navbar.css'
+import Hamburger from '../components/Hamburger.js'
 
-const Navbar = (props) => {
-  
+export default class Navbar extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      lastScrollY: 0,
+      hidden: false,
+      collapsed: false
+    }
+    this.handleScroll = this.handleScroll.bind(this)
+    this.toggleNav = this.toggleNav.bind(this)
+  }
+
+  toggleNav() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    })
+  }
+
+  componentWillMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    const { lastScrollY } = this.state;
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY > lastScrollY && currentScrollY > 100) {
+      this.setState({ hidden: true })
+    } else if (currentScrollY < 100) {
+      this.setState({ hidden: false })
+    }
+  }
+ 
+
+render() {
   let className = 'navigate';
-  if (props.hidden) className += ' hidden';
-  if (props.collapsed) className += ' collapsed';
+  if (this.state.hidden) className += ' hidden';
+  if (this.state.collapsed) className += ' collapsed';
 
   return (
-      <ul className={className} id='navbar'>
-        <li id='home'><NavLink to='/home'>HOME</NavLink></li>
-        <li id='projects'><NavLink to='/projects'>PROJECTS</NavLink></li>
-        <li id='resume'><NavLink to='/resume'>RESUME</NavLink></li>
-        <li id='contacts'><NavLink to='/contacts'>CONTACTS</NavLink></li>
-      </ul>
-  )
+    <div>
+      <Hamburger onClick={this.toggleNav} collapsed={this.state.collapsed} />
+        <ul className={className} id='navbar'>
+          <li id='home'><NavLink to='/home'>HOME</NavLink></li>
+          <li id='projects'><NavLink to='/projects'>PROJECTS</NavLink></li>
+          <li id='resume'><NavLink to='/resume'>RESUME</NavLink></li>
+          <li id='contacts'><NavLink to='/contacts'>CONTACTS</NavLink></li>
+        </ul>
+     </div>
+   
+    )
+  }
 }
 
-export default Navbar
+
--- src/components/Page.js
+++ src/components/Page.js
@@ -1,62 +1,16 @@
 import React from 'react'
 import Navbar from '../components/Navbar.js'
 import Footer from '../components/Footer.js'
-import Hamburger from '../components/Hamburger.js'
 import './styles/page.css'
 
-export default class Page extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      lastScrollY: 0,
-      hidden: false,
-      collapsed: false
-    }
-    this.handleScroll = this.handleScroll.bind(this)
-    this.toggleNav = this.toggleNav.bind(this)
-  }
-  
-
-  toggleNav() {
-    this.setState({
-      collapsed: !this.state.collapsed
-    })
-  }
-
-  componentWillMount() {
-    window.addEventListener('scroll', this.handleScroll);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-
-  handleScroll() {
-    console.log('scrolling', window.scrollY)
-    const { lastScrollY } = this.state;
-    const currentScrollY = window.scrollY;
-
-    if (currentScrollY > lastScrollY && currentScrollY > 100) {
-      this.setState({ hidden: true });
-    } else if (currentScrollY < 100) {
-      this.setState({ hidden: false });
-    }
-  };
-
-  render() {
+const Page = (props) => {
     return (
       <div className="main-content">
-        <div>
-          <Hamburger onClick={this.toggleNav} collapsed={this.state.collapsed} />
-          <Navbar
-            collapsed={this.state.collapsed}
-            hidden={this.state.hidden}
-            handleScroll={this.handleScroll}
-          />
-        </div>
-        {this.props.children}
+        <Navbar/>
+       {props.children}
         <Footer />
       </div>
     )
-  }
 }
+
+export default Page
