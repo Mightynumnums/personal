caedd06a1b00ba257aff8576a98d1e4c58aae72f
Mightynumnums <deadcookies@gmail.com>
2018-07-29 21:12:33 -0400

    Change BrowserRouter to HashRouter for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react'
 import './App.css';
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import { Route, HashRouter as Router, Switch } from "react-router-dom";
 import Home from './components/Home.js'
 import Projects from './components/Projects.js'
 import Resume from './components/Resume.js'
 import Contacts from './components/Contacts.js'
 import LandingPage from './components/LandingPage'
 
-
+const NotFound = () => (
+  <h1>Uhoh. Looks like we couldn't find that page :(</h1>
+);
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div>
+        <Switch>
           <Route exact={true} path='/' component={LandingPage} />
           <Route path='/home' component={Home} />
           <Route path='/projects' component={Projects} />
           <Route path='/resume' component={Resume} />
           <Route path='/contacts' component={Contacts} />
-        </div>
+          <Route component={NotFound} />
+        </Switch>
       </Router >
     );
   }
