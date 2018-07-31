aff26ce37a596614c3f6477c78bd4a887ba4ba14
Mightynumnums <deadcookies@gmail.com>
2018-07-31 17:38:55 -0400

    Footer buttons wired up to go to their desired destinations

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import github from '../images/github.svg'
 import mail from '../images/mail.svg'
 import linked from '../images/linked2.svg'
@@ -10,9 +11,9 @@ export default class Footer extends Component {
     return (
       <div className="footer">
         <div className='footer-logos'>
-          <img src={mail} className="logo" alt="logo" />
-          <img src={github} className="logo" alt="logo" />
-          <img src={linked} className="logo" alt="logo" />
+          <Link to='/contacts'><img src={mail} className="logo" alt="logo" /></Link>
+          <a href='https://github.com/Mightynumnums'><img src={github} className="logo" alt="logo" /></a>
+          <a href='//www.linkedin.com/in/aleksandra-shineleva/'><img src={linked} className="logo" alt="logo" /></a>
         </div>
         <p id='copy'>&copy;Aleks Shineleva | 2018</p>
       </div>
