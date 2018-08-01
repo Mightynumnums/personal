2890dae703b1213418cc9acc558ed29ab43b33f1
Mightynumnums <deadcookies@gmail.com>
2018-08-01 12:37:25 -0400

    Add project header

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,10 +15,12 @@ const Projects = () => {
         <br />
             This website is the first that I've ever built. Ituses HTML and CSS, with some small amount of JavaScript and JQuery. Working on it made me fall in love with programming. It helped me understand how my passion for design and illustration fit together with tech.
         </p>
-          <div className='images'>
-            <img src={anbeL} alt="laptopImage" />
-            <img src={anbeI} alt='iphoneImage' />
-          </div>
+          <a href='https://anbebaccamportfolio.herokuapp.com/'>
+            <div className='images'>
+              <img src={anbeL} alt="laptopImage" />
+              <img src={anbeI} alt='iphoneImage' />
+            </div>
+          </a>
           <div className='projLinks'>
             <a href='https://github.com/Mightynumnums/anbe2'>GITHUB</a>
             <a href='https://anbebaccamportfolio.herokuapp.com/'>WEBSITE</a>
@@ -27,6 +29,7 @@ const Projects = () => {
 
           {/* donke project*/}
           <div id='donkeDiv'>
+            <h2 className='projDescr name'>Donke</h2>
             <p className='projDescr'>The creation of Donke was inspired by inflamed backs and asleep legs.
           <br />
               Donke is a native application created using Electron paltform. It is build with React.js for front end, Redux for state managemet, Green Sock Library for animations, and FireStore for data management and real time updates.
@@ -34,10 +37,12 @@ const Projects = () => {
               My team and I have incorporated a playpen functionality that allows users to interact with other avitars.
               It is a playful application that dreams to help people to be healthier, active and in tern more productive, even if they must sit behind the desk all day.
         </p>
-            <div className='images'>
-              <img src={donke} id="donker" alt="laptopImage" />
-              <iframe id='donke-video' width="460" height="315" title='Donke' src="https://www.youtube.com/embed/xlSb_pJ16g4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            </div>
+            <a href='https://buddy-4f263.firebaseapp.com/'>
+              <div className='images'>
+                <img src={donke} id="donker" alt="laptopImage" />
+                <iframe id='donke-video' width="460" height="315" title='Donke' src="https://www.youtube.com/embed/xlSb_pJ16g4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+              </div>
+            </a>
             <div className='projLinks'>
               <a href='https://github.com/teamCrabby/donke'>GITHUB</a>
               <a href='https://buddy-4f263.firebaseapp.com/'>WEBSITE</a>
