39cd5d0e6db1501ede6a23403f775a9d13c8c5b5
Mightynumnums <deadcookies@gmail.com>
2018-08-02 12:37:18 -0400

    Change the css for HOME component and add another media Q break

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import profilePic from '../images/aleks.jpg'
+import profilePic from '../images/aleksProfl.jpg'
 import Page from './Page'
 import './styles/home.css'
 
@@ -9,16 +9,18 @@ const Home = () => {
       <div className='bio'>
         <div className='bioText'>
           <h2>Welcome!
-          I'm so happy you made it to my site!</h2>
+            I'm so happy you made it to my site!</h2>
           <h3>My name is Aleks and I am a fullstack  software engineer with a keen eye for design.</h3>
           <p>I am an artist by nature and a web developer by training. Art and tech, for me, is a perfect union,- two elements that fulfill and complement one another.
           I love to learn new things, no matter how tough, and I love to teach others (no matter how tough).
           Generally, I like rolling up my sleeves and getting scrappy.
           I currently live, climb, and build apps in Brooklyn, New York.
-          </p>
+            </p>
           <h4>Check out my projects or shoot me a message at the links above!</h4>
         </div>
-        <img src={profilePic} id="profilePic" alt="selfImg" />
+        <div className='imageDiv'>
+          <img src={profilePic} id="profilePic" alt="selfImg" />
+        </div>
       </div>
     </Page>
   );
--- src/components/Page.js
+++ src/components/Page.js
@@ -3,14 +3,24 @@ import Navbar from '../components/Navbar.js'
 import Footer from '../components/Footer.js'
 import './styles/page.css'
 
-const Page = (props) => {
+export default class Page extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  //use this to ensure that the page view always starts off at the top of the page
+  componentDidMount() {
+    window.scrollTo(0, 0)
+  }
+
+  render() {
     return (
       <div className="main-content">
-        <Navbar/>
-       {props.children}
+        <Navbar />
+        {this.props.children}
         <Footer />
       </div>
     )
+  }
 }
-
-export default Page
