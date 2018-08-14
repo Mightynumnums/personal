7c20fe4935507ac55cdf31581fd128dd07e77f9b
Mightynumnums <deadcookies@gmail.com>
2018-08-13 23:00:36 -0400

    Refactor projects component to map content and create projectData file

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,58 +1,44 @@
 import React from 'react'
-import anbeI from '../images/anbeTablet.png'
-import anbeL from '../images/anbeClear.png'
-import donke from '../images/Donke.png'
 import Page from '../components/Page.js'
+import data from '../projectData.js'
 import './styles/projects.css'
 
-const Projects = () => {
-  return (
-    <Page>
-      <div className="projects">
-        <div id='anbePortfolio'>
-          <h2 className='projDescr name'>Anbe Baccam</h2>
-          <p className='projDescr'>Anbe Baccam is an emerging photographer. When I was building his website, I really wanted to showcase his work in a simple way. It was important to me, to let the beauty of his work to speak for itself, without too much disruption or noise.
-        <br />
-            This website is the first that I've ever built. Ituses HTML and CSS, with some small amount of JavaScript and JQuery. Working on it made me fall in love with programming. It helped me understand how my passion for design and illustration fit together with tech.
-        </p>
-          <a href='https://anbebaccamportfolio.herokuapp.com/'>
-            <div className='images'>
-              <img src={anbeL} alt="laptopImage" />
-              <img src={anbeI} alt='iphoneImage' />
-            </div>
-          </a>
-          <div className='projLinks'>
-            <a href='https://github.com/Mightynumnums/anbe2'>GITHUB</a>
-            <a href='https://anbebaccamportfolio.herokuapp.com/'>WEBSITE</a>
-          </div>
+export default class Projects extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      projects: data
+    }
+  }
 
+  render() {
+    return (
+      <Page>
+        <div className='projectMain'>
 
-          {/* donke project*/}
-          <div id='donkeDiv'>
-            <h2 className='projDescr name'>Donke</h2>
-            <p className='projDescr'>The creation of Donke was inspired by inflamed backs and asleep legs.
-          <br />
-              Donke is a native application created using Electron paltform. It is build with React.js for front end, Redux for state managemet, Green Sock Library for animations, and FireStore for data management and real time updates.
-              Donke encourages users to take care of their wellbeing by taking consistent breaks during their work day.
-              My team and I have incorporated a playpen functionality that allows users to interact with other avitars.
-              It is a playful application that dreams to help people to be healthier, active and in tern more productive, even if they must sit behind the desk all day.
-        </p>
-            <a href='https://buddy-4f263.firebaseapp.com/'>
-              <div className='images'>
-                <img src={donke} id="donker" alt="laptopImage" />
-                <iframe id='donke-video' width="460" height="315" title='Donke' src="https://www.youtube.com/embed/xlSb_pJ16g4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          {this.state.projects.map(project => {
+            return (
+              <div className='hover-container' key={project.description}>
+                <div className='imageContainer'>
+                  <img src={project.image.src} alt={project.image.alt} />
+                </div>
+                <div className='image-hover-text-overlay'>
+                  <div className='overlay-content'>
+                    <p>{project.description}</p>
+                  </div>
+                </div>
+                <div className='container projLinks'>
+                  <a href={project.links.ghUrl}>{project.links.github}</a>
+                  <a href={project.links.webUrl}>{project.links.website}</a>
+                </div>
               </div>
-            </a>
-            <div className='projLinks'>
-              <a href='https://github.com/teamCrabby/donke'>GITHUB</a>
-              <a href='https://buddy-4f263.firebaseapp.com/'>WEBSITE</a>
-            </div>
-          </div>
+            )
+          })}
         </div>
-      </div>
-    </Page>
-  );
+      </Page>
+    );
+  }
 }
 
-export default Projects
+
 
